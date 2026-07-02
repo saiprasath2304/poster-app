ebca7904b7a32a16fae0629168d8cfb55046ec61
prasad <prasad@aegisconsulting.in>
2026-07-02 15:42:20 +0530

    img, png, pdf and print - fix for font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
   import '../app.css';
+  import '@fontsource/inter/400.css';
+  import '@fontsource/inter/500.css';
+  import '@fontsource/inter/600.css';
+  import '@fontsource/inter/700.css';
+  import '@fontsource/poppins/600.css';
+  import '@fontsource/poppins/700.css';
+  import '@fontsource/poppins/800.css';
 </script>
 
 <slot />
